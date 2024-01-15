65dc3b19b496f36d1cb8121817bf11b084743a9c
yuri <yurimarques626@gmail.com>
2024-01-15 19:07:30 -0300

    setting styles

---
+++ src/styles/global.js
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+    html{
+        font-size: 62.5%;
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        background-color: ${({ theme }) => theme.colors.background};
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, button, textarea{
+        font-size: 1.6rem;
+        outline: none;
+    }
+
+    a{
+        text-decoration: none;
+    }
+
+    button, a{
+        cursor: pointer;
+        transition: filter .2s;
+    }
+
+    button:hover, a:hover{
+        filter: brightness(.9)
+    }
+
+
+`
---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+export default {
+    colors: {
+        gray: "#999591",
+        pink: "#FF859B",
+        backgroundTag: "#312E38",
+        textTag: "#E5E5E5",
+        textColor: "#F4EDE8",
+        background: "#1C1B1E",
+        backgroundNote: "#FF859B0D",
+        lineColor: "#3E3B47",
+        details: "#948F99",
+        black: "#000000",
+        inputColor: "#262529",
+        white: "#FFFFFF"
+
+    }
+}
