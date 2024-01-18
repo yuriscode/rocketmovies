66f72d532c79d83ba2d159905a2889712446ef23
yuri <yurimarques626@gmail.com>
2024-01-18 20:48:14 -0300

    rest operator

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,12 @@
 import { Container } from './styles';
 
-export function Button({title, loading}){
+export function Button({title, loading=false, ...rest}){
     return(
-        <Container type='button' disabled={loading}>
+        <Container 
+            type='button' 
+            disabled={loading} 
+            {...rest}
+        >
             {loading ? 'Carregando...' : title}
         </Container>
     )
